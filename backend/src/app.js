962192de36a81cf3f261309a31289270e20fd63d
vescio <rafael.vescio@sensedata.com.br>
2020-03-27 10:00:44 -0300

    add testes e validações

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes');
 
 
@@ -9,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+app.use(errors());
 /**
  * Metodos HTTP:
  * GET: Buscar/Listar uma informação do back-end
@@ -34,4 +36,4 @@ app.use(routes);
     * Query Builder: table('users').select('*').where()
     */
 
-app.listen(3333);
+module.exports = app;
